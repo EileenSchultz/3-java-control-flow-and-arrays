06f04cfb7da2ec805a1077b6eeec0e4acdb0bad1
NilanSeon <148245767+EileenSchultz@users.noreply.github.com>
2026-09-14 13:35:25 +0300

    Колесова Алёна. Задания 1-13

--- task10/src/main/java/com/example/task10/Task10Main.java
+++ task10/src/main/java/com/example/task10/Task10Main.java
@@ -2,9 +2,10 @@ package com.example.task10;
 
 public class Task10Main {
     public static void main(String[] args) {
-
+        /*
         int[] arr = {7, 5, 9};
         System.out.println(numMin(arr));
+        */
 
     }
 
